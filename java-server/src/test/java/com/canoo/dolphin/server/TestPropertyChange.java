04e74a79fbca2447a2a3ee44d851d4fbc34efa15
Hendrik Ebbers <hendrik.ebbers@web.de>
2015-03-30 21:38:01 +0200

    Test für PropertyChanged

--- java-server/src/test/java/com/canoo/dolphin/server/TestPropertyChange.java
+++ java-server/src/test/java/com/canoo/dolphin/server/TestPropertyChange.java
@@ -1,7 +1,65 @@
 package com.canoo.dolphin.server;
 
+import com.canoo.dolphin.mapping.ValueChangeEvent;
+import com.canoo.dolphin.mapping.ValueChangeListener;
+import com.canoo.dolphin.server.util.AbstractDolphinBasedTest;
+import com.canoo.dolphin.server.util.SimpleTestModel;
+import org.junit.Test;
+import org.opendolphin.core.Attribute;
+import org.opendolphin.core.server.ServerDolphin;
+import org.opendolphin.core.server.ServerPresentationModel;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by hendrikebbers on 30.03.15.
  */
-public class TestPropertyChange {
+public class TestPropertyChange extends AbstractDolphinBasedTest {
+
+    private String newValue;
+
+    private String oldValue;
+
+    private boolean listenerCalled;
+
+    @Test
+    public void testWithAnnotatedSimpleModel() {
+        ServerDolphin dolphin = createServerDolphin();
+        BeanManager manager = new BeanManager(dolphin);
+
+        final SimpleTestModel model = manager.create(SimpleTestModel.class);
+
+        ValueChangeListener<String> myListener = new ValueChangeListener<String>() {
+            @Override
+            public void valueChanged(ValueChangeEvent<? extends String> evt) {
+                assertEquals(model.getTextProperty(), evt.getSource());
+                newValue = evt.getNewValue();
+                oldValue = evt.getOldValue();
+                listenerCalled = true;
+            }
+        };
+
+        model.getTextProperty().addValueListener(myListener);
+        assertEquals(null, newValue);
+        assertEquals(null, oldValue);
+        assertEquals(false, listenerCalled);
+
+        model.getTextProperty().set("Hallo Property");
+        assertEquals("Hallo Property", newValue);
+        assertEquals(null, oldValue);
+        assertEquals(true, listenerCalled);
+
+        listenerCalled = false;
+        model.getTextProperty().set("Hallo Property2");
+        assertEquals("Hallo Property2", newValue);
+        assertEquals("Hallo Property", oldValue);
+        assertEquals(true, listenerCalled);
+
+        listenerCalled = false;
+        model.getTextProperty().removeValueListener(myListener);
+        model.getTextProperty().set("Hallo Property3");
+        assertEquals("Hallo Property2", newValue);
+        assertEquals("Hallo Property", oldValue);
+        assertEquals(false, listenerCalled);
+    }
 }
