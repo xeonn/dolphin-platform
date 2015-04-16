1ce1c5f7a83f950de99a05e6db5d231fcd506d40
Michael Heinrichs <michael.heinrichs@netopyr.com>
2015-04-16 17:00:39 +0200

    New constant for Command Parameters

--- java-server/src/main/java/com/canoo/dolphin/server/impl/DolphinConstants.java
+++ java-server/src/main/java/com/canoo/dolphin/server/impl/DolphinConstants.java
@@ -12,6 +12,8 @@ public final class DolphinConstants {
     public static final String DEL_FROM_CLIENT = "@@@ LIST_DEL_FROM_CLIENT @@@";
     public static final String SET_FROM_CLIENT = "@@@ LIST_SET_FROM_CLIENT @@@";
 
+    public static final String DOLPHIN_PARAMETER = "@@@ DOLPHIN_PARAMETER @@@";
+
     private DolphinConstants() {}
 
 
