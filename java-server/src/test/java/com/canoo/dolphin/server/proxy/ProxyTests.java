1eb2cf97d5d2bb7f582f7b5cf211c9dd21e8dd71
Fabian Dreier <fabian.dreier@canoo.com>
2015-04-16 18:38:35 +0200

    improved tests

--- java-server/src/test/java/com/canoo/dolphin/server/proxy/ProxyTests.java
+++ java-server/src/test/java/com/canoo/dolphin/server/proxy/ProxyTests.java
@@ -2,18 +2,22 @@ package com.canoo.dolphin.server.proxy;
 
 import com.canoo.dolphin.server.impl.BeanRepository;
 import com.canoo.dolphin.server.impl.ClassRepository;
+import com.canoo.dolphin.server.impl.DolphinConstants;
 import com.canoo.dolphin.server.impl.collections.ListMapper;
 import com.canoo.dolphin.server.util.AbstractDolphinBasedTest;
+import org.opendolphin.core.Attribute;
 import org.opendolphin.core.server.ServerDolphin;
+import org.opendolphin.core.server.ServerPresentationModel;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import javax.swing.*;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.empty;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
@@ -50,19 +54,40 @@ public class ProxyTests extends AbstractDolphinBasedTest {
         car = factory.create(TestCarModel.class);
         assertNotNull(car);
 
-        car.getBrandNameProperty().set("a String");
-        assertEquals("a String", car.getBrandNameProperty().get());
+        String expectedValue = "a String";
+
+        car.getBrandNameProperty().set(expectedValue);
+        assertEquals(expectedValue, car.getBrandNameProperty().get());
+
+
+        assertCorrectPM("brandName", expectedValue, TestCarModel.class.getName(), 4);
     }
 
     @Test
     public void proxyInstance_Inheritance() {
         TestCarManufacturer manufacturer = factory.create(TestCarManufacturer.class);
 
-        manufacturer.setName("a Name");
+        String expectedValue = "a Name";
 
-        assertThat(manufacturer.getName(), is("a Name"));
+        manufacturer.setName(expectedValue);
+
+        assertThat(manufacturer.getName(), is(expectedValue));
+
+        assertCorrectPM("name", expectedValue, TestCarManufacturer.class.getName(), 3);
     }
 
+    private void assertCorrectPM(String attributeName, String attributeValue, String modelClass, int expectedAttributeSize) {
+        List<ServerPresentationModel> dolphinModels = dolphin.findAllPresentationModelsByType(modelClass);
+        assertThat(dolphinModels, hasSize(1));
+
+        ServerPresentationModel dolphinModel = dolphinModels.get(0);
+
+        List<Attribute> attributes = dolphinModel.getAttributes();
+        assertThat(attributes, hasSize(expectedAttributeSize));
+
+        Attribute textAttribute = dolphinModel.getAt(attributeName);
+        assertThat(textAttribute.getValue().toString(), is(attributeValue));
+    }
 
     @Test
     public void proxyInstanceWithSetter() {
@@ -72,6 +97,8 @@ public class ProxyTests extends AbstractDolphinBasedTest {
         car.setYear(2015);
 
         assertThat(car.getYear(), is(2015));
+
+        assertCorrectPM("year", "2015", TestCarModel.class.getName(), 4);
     }
 
     @Test
@@ -81,6 +108,11 @@ public class ProxyTests extends AbstractDolphinBasedTest {
         car.getTripKilometerCounters().addAll(Arrays.asList(1, 3));
 
         assertThat(car.getTripKilometerCounters(), contains(1,3));
+
+        List<ServerPresentationModel> changes = dolphin.findAllPresentationModelsByType(DolphinConstants.ADD_FROM_SERVER);
+        assertThat(changes, hasSize(2));
+        assertThat(dolphin.findAllPresentationModelsByType(DolphinConstants.DEL_FROM_SERVER), empty());
+        assertThat(dolphin.findAllPresentationModelsByType(DolphinConstants.SET_FROM_SERVER), empty());
     }
 
     @Test
@@ -101,6 +133,23 @@ public class ProxyTests extends AbstractDolphinBasedTest {
         assertThat(manufacturer.getName(),is("name"));
         assertThat(manufacturer.getCapital(),is("capital"));
         assertThat(manufacturer.getNation(),is("schwiiz"));
+
+        List<ServerPresentationModel> carModels = dolphin.findAllPresentationModelsByType(TestCarModel.class.getName());
+        assertThat(carModels, hasSize(1));
+        List<ServerPresentationModel> manufacturerModels = dolphin.findAllPresentationModelsByType(TestCarManufacturer.class.getName());
+        assertThat(manufacturerModels, hasSize(1));
+
+    }
+
+    @Test
+    public void proxyInstanceWithSomeInterface() {
+        car = factory.create(TestCarModel.class);
+        assertNotNull(car);
+
+        Action action = factory.create(Action.class);
+        action.setEnabled(true);
+
+        assertCorrectPM("enabled", "true", Action.class.getName(), 1);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class,expectedExceptionsMessageRegExp = "Getter for property brandName should end with \"Property\"")
