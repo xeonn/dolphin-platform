f61798f10856c7c65ec1b27ee102803de74276eb
Hendrik Ebbers <hendrik.ebbers@web.de>
2015-04-16 16:01:01 +0200

    demo: removed unneeded stuff

--- spring-boot-example/src/main/java/com/canoo/dolphin/demo/spring/DemoController.java
+++ spring-boot-example/src/main/java/com/canoo/dolphin/demo/spring/DemoController.java
@@ -3,9 +3,7 @@ package com.canoo.dolphin.demo.spring;
 import com.canoo.dolphin.server.BeanManager;
 import com.canoo.dolphin.server.DolphinAction;
 import com.canoo.dolphin.server.DolphinController;
-import org.opendolphin.core.server.ServerDolphin;
 
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 @DolphinController
@@ -13,14 +11,6 @@ public class DemoController {
 
     @Inject
     private BeanManager manager;
-
-    @Inject
-    private ServerDolphin dolphin;
-
-    @PostConstruct
-    public void bla() {
-        System.out.println("");
-    }
 
     @DolphinAction
     public void callMe() {
