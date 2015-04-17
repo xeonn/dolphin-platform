96c07da122991f1b5007663b4a450f487f79768d
Hendrik Ebbers <hendrik.ebbers@web.de>
2015-04-17 09:26:42 +0200

    bugfix

--- java-server/src/main/java/com/canoo/dolphin/server/event/impl/PollingController.java
+++ java-server/src/main/java/com/canoo/dolphin/server/event/impl/PollingController.java
@@ -2,6 +2,8 @@ package com.canoo.dolphin.server.event.impl;
 
 import com.canoo.dolphin.server.event.DolphinEventBus;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by hendrikebbers on 17.04.15.
  */
@@ -9,7 +11,7 @@ public class PollingController {
 
     public void pollEvents() {
         try {
-            DolphinEventBusImpl.getInstance().sendEventsForCurrentDolphinSession();
+            DolphinEventBusImpl.getInstance().sendEventsForCurrentDolphinSession(10, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             //this exception is called when the clients has an update and needs to stop the polling.
             // Therefore we do nothing here
