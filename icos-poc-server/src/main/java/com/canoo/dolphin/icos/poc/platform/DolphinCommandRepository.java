023e75a2b21ec70b1ad55866ff041e0b1308347e
Michael Heinrichs <michael.heinrichs@netopyr.com>
2015-05-20 11:44:38 +0200

    Fix for broken example

--- icos-poc-server/src/main/java/com/canoo/dolphin/icos/poc/platform/DolphinCommandRepository.java
+++ icos-poc-server/src/main/java/com/canoo/dolphin/icos/poc/platform/DolphinCommandRepository.java
@@ -1,8 +1,10 @@
 package com.canoo.dolphin.icos.poc.platform;
 
+import org.opendolphin.core.comm.Command;
 import org.opendolphin.core.server.ServerDolphin;
 import org.opendolphin.core.server.action.DolphinServerAction;
 import org.opendolphin.core.server.comm.ActionRegistry;
+import org.opendolphin.core.server.comm.CommandHandler;
 import org.reflections.Reflections;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +15,8 @@ import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 @Service
 public class DolphinCommandRepository {
@@ -31,24 +35,37 @@ public class DolphinCommandRepository {
         Reflections reflections = new Reflections(mainPackage);
         Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(DolphinCommand.class);
         commandClasses = new ArrayList<>();
-        annotated.stream().filter(AbstractDolphinCommand.class::isAssignableFrom).forEach(c -> commandClasses.add((Class<AbstractDolphinCommand>) c));
+        annotated.stream().filter(new Predicate<Class<?>>() {
+            @Override
+            public boolean test(Class<?> cls) {
+                return AbstractDolphinCommand.class.isAssignableFrom(cls);
+            }
+        }).forEach(new Consumer<Class<?>>() {
+            @Override
+            public void accept(Class<?> cls) {
+                commandClasses.add((Class<AbstractDolphinCommand>) cls);
+            }
+        });
     }
 
-    public void initCommandsForDolphin(ServerDolphin dolphin) {
+    public void initCommandsForDolphin(final ServerDolphin dolphin) {
         dolphin.register(new DolphinServerAction() {
             @Override
             public void registerIn(ActionRegistry registry) {
                 //Alle registrieren
                 for (Class<AbstractDolphinCommand> commandClass : commandClasses) {
-                    AbstractDolphinCommand handler = applicationContext.getAutowireCapableBeanFactory().createBean(commandClass);
+                    final AbstractDolphinCommand handler = applicationContext.getAutowireCapableBeanFactory().createBean(commandClass);
                     handler.setDolphin(dolphin);
 
                     DolphinCommand commandAnnotation = handler.getClass().getAnnotation(DolphinCommand.class);
-                    registry.register(commandAnnotation.value(), (command, list) -> {
-                        try {
-                            handler.action();
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                    registry.register(commandAnnotation.value(), new CommandHandler() {
+                        @Override
+                        public void handleCommand(Command command, List list) {
+                            try {
+                                handler.action();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
                     });
                 }
