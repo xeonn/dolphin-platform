a5e8ea481c9d49d6ac9147b433933c33e35e369c
Hendrik Ebbers <hendrik.ebbers@web.de>
2016-01-11 12:59:18 +0100

    Exception Type

--- java-client/src/main/java/com/canoo/dolphin/client/ClientInitializationException.java
+++ java-client/src/main/java/com/canoo/dolphin/client/ClientInitializationException.java
@@ -3,7 +3,7 @@ package com.canoo.dolphin.client;
 /**
  * Created by hendrikebbers on 11.01.16.
  */
-public class ClientInitializationException extends Exception {
+public class ClientInitializationException extends RuntimeException {
 
     public ClientInitializationException(String message) {
         super(message);
