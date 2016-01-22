b8205d9e83fa2fffacc929cb5fa3d4b37142df43
Hendrik Ebbers <hendrik.ebbers@web.de>
2016-01-22 16:29:07 +0100

    changes based on review

--- java-bean-validation/src/main/java/com/canoo/dolphin/validation/NotNullPropertyValidator.java
+++ java-bean-validation/src/main/java/com/canoo/dolphin/validation/NotNullPropertyValidator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015-2016 Canoo Engineering AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.canoo.dolphin.validation;
 
 import com.canoo.dolphin.mapping.Property;
@@ -6,7 +21,7 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import javax.validation.constraints.NotNull;
 
-public class NotNullPropertyValidator implements ConstraintValidator<NotNull, Property> {
+public final class NotNullPropertyValidator implements ConstraintValidator<NotNull, Property> {
 
     @Override
     public void initialize(NotNull constraintAnnotation) {
@@ -15,10 +30,7 @@ public class NotNullPropertyValidator implements ConstraintValidator<NotNull, Pr
     @Override
     public boolean isValid(Property value,
                            ConstraintValidatorContext context) {
-        if (value == null) {
-            return false;
-        }
-        if (value.get() == null) {
+        if (value == null || value.get() == null) {
             return false;
         }
         return true;
