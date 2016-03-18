f70d79cb3276b6ef6902179cfa449b2ef1c068c1
Hendrik Ebbers <hendrik.ebbers@web.de>
2016-03-18 11:18:39 +0100

    Test, tests, tests

---
+++ java-server/src/test/java/com/canoo/dolphin/server/context/DolphinContextHandlerTest.java
@@ -0,0 +1,15 @@
+package com.canoo.dolphin.server.context;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by hendrikebbers on 18.03.16.
+ */
+public class DolphinContextHandlerTest {
+
+    @Test
+    public void test123() {
+
+    }
+
+}
