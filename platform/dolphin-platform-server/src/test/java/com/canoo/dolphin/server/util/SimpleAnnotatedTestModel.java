59628ab8391e6284424c10c765de4cf58cdec1da
Hendrik Ebbers <hendrik.ebbers@web.de>
2016-08-18 14:14:59 +0200

    Based on review

--- platform/dolphin-platform-server/src/test/java/com/canoo/dolphin/server/util/SimpleAnnotatedTestModel.java
+++ platform/dolphin-platform-server/src/test/java/com/canoo/dolphin/server/util/SimpleAnnotatedTestModel.java
@@ -16,16 +16,11 @@
 package com.canoo.dolphin.server.util;
 
 import com.canoo.dolphin.mapping.DolphinBean;
-import com.canoo.dolphin.mapping.DolphinProperty;
 import com.canoo.dolphin.mapping.Property;
 
-/**
- * Created by hendrikebbers on 30.03.15.
- */
-@DolphinBean("simple_test_model")
+@DolphinBean
 public class SimpleAnnotatedTestModel {
 
-    @DolphinProperty("text_property")
     private Property<String> skdjfhksdjhf;
 
     public Property<String> getTextProperty() {
