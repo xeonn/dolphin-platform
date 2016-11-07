322fb0001ad14b16ee1ce86c664255a5b5c86896
Hendrik Ebbers <hendrik.ebbers@web.de>
2016-11-07 13:11:50 +0100

    Groovy  -> Java

---
+++ remoting/dolphin-remoting-client/src/main/groovy/org/opendolphin/core/client/ClientPresentationModel.java
@@ -0,0 +1,39 @@
+package org.opendolphin.core.client;
+
+import org.codehaus.groovy.runtime.StringGroovyMethods;
+import org.opendolphin.core.BasePresentationModel;
+
+import java.util.List;
+
+public final class ClientPresentationModel extends BasePresentationModel<ClientAttribute> {
+    public ClientPresentationModel(List<ClientAttribute> attributes) {
+        this(null, attributes);
+    }
+
+    /**
+     * @param id if id is null or empty, an auto-generated id will be used
+     */
+    public ClientPresentationModel(String id, List<ClientAttribute> attributes) {
+        super((id != null && StringGroovyMethods.asBoolean(id)) ? id : "" + instanceCount++ + AUTO_ID_SUFFIX, attributes);
+        if (id != null && id.endsWith(AUTO_ID_SUFFIX)) {
+            throw new IllegalArgumentException("presentation model with self-provided id \'" + id + "\' may not end with suffix \'" + AUTO_ID_SUFFIX + "\' since that is reserved.");
+        }
+
+    }
+
+    public boolean getClientSideOnly() {
+        return clientSideOnly;
+    }
+
+    public boolean isClientSideOnly() {
+        return clientSideOnly;
+    }
+
+    public void setClientSideOnly(boolean clientSideOnly) {
+        this.clientSideOnly = clientSideOnly;
+    }
+
+    public static final String AUTO_ID_SUFFIX = "-AUTO-CLT";
+    private boolean clientSideOnly = false;
+    private static long instanceCount = 0;
+}
