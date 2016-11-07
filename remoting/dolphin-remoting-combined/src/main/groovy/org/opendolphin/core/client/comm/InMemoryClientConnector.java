dc251400e99e21060c1596a99294e9726e046f19
Hendrik Ebbers <hendrik.ebbers@web.de>
2016-11-07 12:56:49 +0100

    Groovy  -> Java

--- remoting/dolphin-remoting-combined/src/main/groovy/org/opendolphin/core/client/comm/InMemoryClientConnector.java
+++ remoting/dolphin-remoting-combined/src/main/groovy/org/opendolphin/core/client/comm/InMemoryClientConnector.java
@@ -1,36 +1,17 @@
-/*
- * Copyright 2015-2016 Canoo Engineering AG.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.opendolphin.core.client.comm
-import groovy.util.logging.Log
-import org.codehaus.groovy.runtime.DefaultGroovyMethods
-import org.opendolphin.core.client.ClientDolphin
-import org.opendolphin.core.comm.Command
-import org.opendolphin.core.server.ServerConnector
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+package org.opendolphin.core.client.comm;
 
-@Log
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
+import org.opendolphin.core.client.ClientDolphin;
+import org.opendolphin.core.comm.Command;
+import org.opendolphin.core.server.ServerConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class InMemoryClientConnector extends AbstractClientConnector {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(InMemoryClientConnector.class);
-
-    private final ServerConnector serverConnector;
-
-    private long sleepMillis = 0;
-
     public InMemoryClientConnector(ClientDolphin clientDolphin, ServerConnector serverConnector) {
         super(clientDolphin);
         this.serverConnector = serverConnector;
@@ -49,6 +30,7 @@ public class InMemoryClientConnector extends AbstractClientConnector {
             return Collections.EMPTY_LIST;
         }
 
+
         if (sleepMillis > 0) {
             try {
                 Thread.sleep(sleepMillis);
@@ -56,13 +38,16 @@ public class InMemoryClientConnector extends AbstractClientConnector {
                 throw new RuntimeException(e);
             }
 
+
         }
+
 
         List<Command> result = new LinkedList<Command>();
         for (Command command : commands) {
             LOGGER.trace("processing {}", command);
-            result.addAll(serverConnector.receive(command));// there is no need for encoding since we are in-memory
+            ((LinkedList<Command>) result).addAll(serverConnector.receive(command));// there is no need for encoding since we are in-memory
         }
+
 
         return result;
     }
@@ -74,4 +59,8 @@ public class InMemoryClientConnector extends AbstractClientConnector {
     public void setSleepMillis(long sleepMillis) {
         this.sleepMillis = sleepMillis;
     }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InMemoryClientConnector.class);
+    private final ServerConnector serverConnector;
+    private long sleepMillis = 0;
 }
