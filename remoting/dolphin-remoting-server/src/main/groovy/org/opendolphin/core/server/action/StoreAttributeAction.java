1690df61262a7afcc12bc280346d01fb97aab0f4
Hendrik Ebbers <hendrik.ebbers@web.de>
2016-12-19 16:53:09 +0100

    Groovy -> Java

---
+++ remoting/dolphin-remoting-server/src/main/groovy/org/opendolphin/core/server/action/StoreAttributeAction.java
@@ -0,0 +1,61 @@
+package org.opendolphin.core.server.action;
+
+import groovy.lang.Closure;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
+import org.opendolphin.core.Attribute;
+import org.opendolphin.core.PresentationModel;
+import org.opendolphin.core.comm.AttributeCreatedNotification;
+import org.opendolphin.core.comm.ChangeAttributeMetadataCommand;
+import org.opendolphin.core.server.ServerAttribute;
+import org.opendolphin.core.server.ServerModelStore;
+import org.opendolphin.core.server.ServerPresentationModel;
+import org.opendolphin.core.server.comm.ActionRegistry;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class StoreAttributeAction extends DolphinServerAction {
+    public void registerIn(ActionRegistry registry) {
+        registry.register(AttributeCreatedNotification.class, new Closure<Object>(this, this) {
+            public void doCall(AttributeCreatedNotification command, Object response) {
+                ServerModelStore modelStore = getServerDolphin().getServerModelStore();
+                Attribute existing = modelStore.findAttributeById(command.getAttributeId());
+                if (DefaultGroovyMethods.asBoolean(existing)) {
+                    LOG.info("trying to store an already existing attribute: " + command);
+                    return;
+                }
+
+                ServerAttribute attribute = new ServerAttribute(command.getPropertyName(), command.getNewValue(), command.getQualifier(), command.getTag());
+                attribute.setId(command.getAttributeId());
+                PresentationModel pm = getServerDolphin().findPresentationModelById(command.getPmId());
+                if (pm == null) {
+                    pm = new ServerPresentationModel(command.getPmId(), new ArrayList(), modelStore);
+                    modelStore.add(pm);
+                }
+                ((ServerPresentationModel) pm)._internal_addAttribute(attribute);
+                modelStore.registerAttribute(attribute);
+            }
+
+        });
+
+        registry.register(ChangeAttributeMetadataCommand.class, new Closure<Object>(this, this) {
+            public void doCall(final ChangeAttributeMetadataCommand command, Object response) {
+                final Attribute attribute = getServerDolphin().findAttributeById(command.getAttributeId());
+                if (!DefaultGroovyMethods.asBoolean(attribute)) {
+                    LOG.warning("Cannot find attribute with id '" + command.getAttributeId() + "'. Metadata remains unchanged.");
+                    return;
+                }
+
+                ((ServerAttribute) attribute).silently(new Runnable() {
+                    @Override
+                    public void run() {
+                        DefaultGroovyMethods.putAt(attribute, command.getMetadataName(), command.getValue());
+                    }
+
+                });
+            }
+        });
+    }
+
+    private static final Logger LOG = Logger.getLogger(StoreAttributeAction.class.getName());
+}
---
+++ remoting/dolphin-remoting-server/src/main/groovy/org/opendolphin/core/server/action/StoreValueChangeAction.java
@@ -0,0 +1,38 @@
+package org.opendolphin.core.server.action;
+
+import groovy.lang.Closure;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
+import org.opendolphin.core.comm.ValueChangedCommand;
+import org.opendolphin.core.server.ServerAttribute;
+import org.opendolphin.core.server.comm.ActionRegistry;
+
+import java.util.logging.Logger;
+
+public class StoreValueChangeAction extends DolphinServerAction {
+    public void registerIn(ActionRegistry registry) {
+        registry.register(ValueChangedCommand.class, new Closure<Object>(this, this) {
+            public void doCall(final ValueChangedCommand command, Object response) {
+                final ServerAttribute attribute = getServerDolphin().findAttributeById(command.getAttributeId());
+                if (DefaultGroovyMethods.asBoolean(attribute)) {
+                    if ((attribute.getValue() != null || command.getOldValue() != null) && !attribute.getValue().equals(command.getOldValue())) {
+                        LOG.warning("S: updating attribute with id '" + command.getAttributeId() + "' to new value '" + command.getNewValue() + "' even though its old command value '" + command.getOldValue() + "' does not conform to the old value of '" + attribute.getValue() + "'. Client overrules server.");
+                    }
+
+                    attribute.silently(new Runnable() {
+                        @Override
+                        public void run() {
+                            attribute.setValue(command.getNewValue());
+                        }
+
+                    });
+                } else {
+                    LOG.severe("S: cannot find attribute with id '" + command.getAttributeId() + "' to change value from '" + command.getOldValue() + "' to '" + command.getNewValue() + "'.");
+                }
+
+            }
+
+        });
+    }
+
+    private static final Logger LOG = Logger.getLogger(StoreValueChangeAction.class.getName());
+}
