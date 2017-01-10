35672002d1bb78f750660799c0e6e1eba83e2efe
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-01-10 15:20:16 +0100

    based on review

--- platform-extras/dolphin-platform-bean-validation/src/main/java/com/canoo/dolphin/validation/AssertTruePropertyValidator.java
+++ platform-extras/dolphin-platform-bean-validation/src/main/java/com/canoo/dolphin/validation/AssertTruePropertyValidator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2016 Canoo Engineering AG.
+ * Copyright 2015-2017 Canoo Engineering AG.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
