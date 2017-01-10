bc7c3cb0c6d1a87f0dbee7275a7951f9d69e4560
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-01-10 15:44:26 +0100

    based on review

--- platform/dolphin-platform-client/src/main/java/com/canoo/dolphin/client/impl/DolphinHttpResponseException.java
+++ platform/dolphin-platform-client/src/main/java/com/canoo/dolphin/client/impl/DolphinHttpResponseException.java
@@ -1,8 +1,5 @@
 package com.canoo.dolphin.client.impl;
 
-/**
- * Created by hendrikebbers on 28.12.16.
- */
 public class DolphinHttpResponseException extends Exception {
 
     public DolphinHttpResponseException(int statusCode, String message) {
