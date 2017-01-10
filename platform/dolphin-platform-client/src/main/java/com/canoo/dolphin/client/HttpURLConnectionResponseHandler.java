f7f4750f8daef8cfdefbdcd7b3b79b13faec8189
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-01-10 15:42:49 +0100

    based on review

--- platform/dolphin-platform-client/src/main/java/com/canoo/dolphin/client/HttpURLConnectionResponseHandler.java
+++ platform/dolphin-platform-client/src/main/java/com/canoo/dolphin/client/HttpURLConnectionResponseHandler.java
@@ -2,9 +2,6 @@ package com.canoo.dolphin.client;
 
 import java.net.HttpURLConnection;
 
-/**
- * Created by hendrikebbers on 27.12.16.
- */
 public interface HttpURLConnectionResponseHandler {
 
     void handle(HttpURLConnection response);
